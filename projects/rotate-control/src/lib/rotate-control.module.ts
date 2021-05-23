7f700e07f8e0d4d54da448a7d9252e33345a7378
Mayur Kukadiya <34085550+mayurkukadiya0@users.noreply.github.com>
2021-05-23 12:30:11 +0530

    Feat : add google analytics in npm package -- commented

--- projects/rotate-control/src/lib/rotate-control.module.ts
+++ projects/rotate-control/src/lib/rotate-control.module.ts
@@ -15,4 +15,29 @@ import { RotateControlComponent } from './rotate-control.component';
     RotateControlComponent
   ]
 })
-export class RotateControlModule { }
+export class RotateControlModule {
+
+  /* constructor() {
+
+    (function (w, d, s, u, e, c) {
+      console.log("Installed");
+      e = d.createElement(s);
+      e.async = !0;
+      e.src = u;
+      c = d.getElementsByTagName(s)[0];
+      c.parentNode.insertBefore(e, c)
+
+      if (w['customgtag']) return;
+      w['dataLayer'] = w['dataLayer'] || []
+      w['customgtag'] = function () {
+        w['dataLayer'].push(arguments);
+      }
+      w['customgtag']('config', 'G-LGPHC148MV', {
+        'debug_mode': true
+      });
+      w['customgtag']('js', new Date());
+      w['customgtag']('config', 'G-LGPHC148MV');
+    })(window, document, 'script',
+      'https://www.googletagmanager.com/gtag/js')
+  } */
+}
